4c3c1135cac92431db84a8b405e34dea2ea182cf
Mad Wiki <wudifengxiang@gmail.com>
2025-01-10 21:38:22 +0800

    remove useless scripts

--- arguments.js
+++
@@ -1,8 +0,0 @@
-// LaunchPool constructor parameters
-module.exports = [
-  "0x67330f6BC8dcE05816662785A89fb0611F6D149F", // stakedToken - staked token address
-  "300000000000000000000", // poolRewardAmount - total reward amount for this pool (300 tokens with 18 decimals)
-  "100000000", // poolLimitPerUser - user limit 100 tokens (18 decimals)
-  "1000000", // minStakeAmount - minimum stake amount 0.1 token (18 decimals)
-  "0", // projectId - project ID
-];
